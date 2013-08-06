bcb5e72e2917d25bffa3bf6a12197c365587af99
Gerhard <potgieterg@gmail.com>
2013-08-06 16:12:40 +0200

    Include template file for certificate

---
+++ templates/certificate.php
@@ -0,0 +1,9 @@
+ <?php
+if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
+
+do_action( 'sensei_certificate_before' );
+
+do_action( 'sensei_certificate' );
+
+do_action( 'sensei_certificate_after' );
+?>
