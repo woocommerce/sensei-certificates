eb90e86c4bc78e321176948a51215bfe2e64f81d
Gerhard <potgieterg@gmail.com>
2013-08-19 21:06:33 +0200

    Remove template file as we now use enpoints

--- templates/certificate.php
+++
@@ -1,9 +0,0 @@
- <?php
-if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
-
-do_action( 'sensei_certificate_before' );
-
-do_action( 'sensei_certificate' );
-
-do_action( 'sensei_certificate_after' );
-?>
