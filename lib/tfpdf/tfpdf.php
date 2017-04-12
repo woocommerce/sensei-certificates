2e5f6b494956551f44db7132cc324faab48b043b
Panos Kountanis <panosktn@gmail.com>
2017-04-13 02:22:29 +0300

    Add phpcs ignoreLine to a set_magic_quotes_runtime call that will never happen in PHP 7

--- lib/tfpdf/tfpdf.php
+++ lib/tfpdf/tfpdf.php
@@ -1260,8 +1260,11 @@ function _dochecks()
 	if(ini_get('mbstring.func_overload') & 2)
 		$this->Error('mbstring overloading must be disabled');
 	// Ensure runtime magic quotes are disabled
-	if(get_magic_quotes_runtime())
+	if( function_exists( 'get_magic_quotes_runtime' ) && get_magic_quotes_runtime() && function_exists( 'set_magic_quotes_runtime' ) ) {
+		// @codingStandardsIgnoreLine
 		@set_magic_quotes_runtime(0);
+	}
+
 }
 
 function _getfontpath()
