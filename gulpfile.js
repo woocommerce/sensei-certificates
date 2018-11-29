b5385b5a60db0ab8c03ea2cedee30a05f698ccf7
Jake Oehler Morrison <jake.morrison@automattic.com>
2018-11-29 17:03:21 +0000

    Add gulp packaging for release

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,45 @@
 var gulp      = require( 'gulp' );
 var wpPot     = require( 'gulp-wp-pot' );
 var sort      = require( 'gulp-sort' );
+var zip       = require( 'gulp-zip' );
 
-gulp.task( 'pot', function() {
-        return gulp.src( [ '**/**.php', '!node_modules/**'] )
-                .pipe( sort() )
-                .pipe( wpPot({
-                        domain: 'sensei-certificates'
-                }) )
-                .pipe( gulp.dest( 'lang' ) );
-});
+var paths = {
+    packageContents: [
+        'admin/**/*',
+        'assets/**/*',
+        'classes/**/*',
+        'lang/**/*',
+        'lib/**/*',
+        'templates/**/*',
+        'woo-includes/**/*',
+        'changelog.txt',
+        'LICENSE',
+        'README.md',
+        'templates/**/*',
+        'woothemes-sensei-certificates.php',
+    ],
+    packageDir: 'build/sensei-certificates',
+    packageZip: 'build/sensei-certificates.zip'
+};
+
+gulp.task( 'pot', gulp.series( function() {
+    return gulp.src( [ '**/**.php', '!node_modules/**', '!build/**' ] )
+        .pipe( sort() )
+        .pipe( wpPot( {
+            domain: 'sensei-certificates'
+        } ) )
+        .pipe( gulp.dest( 'lang/sensei-certificates.pot' ) );
+} ) );
+
+gulp.task( 'copy-package', function() {
+    return gulp.src( paths.packageContents, { base: '.' } )
+        .pipe( gulp.dest( paths.packageDir ) );
+} );
+
+gulp.task( 'zip-package', function() {
+    return gulp.src( paths.packageDir + '/**/*', { base: paths.packageDir + '/..' } )
+        .pipe( zip( paths.packageZip ) )
+        .pipe( gulp.dest( '.' ) );
+} );
+
+gulp.task( 'package', gulp.series( 'copy-package', 'zip-package' ) );
