de82c5f229e3bbe7aa6602bc3e6e02de9be9ae75
Jake Oehler Morrison <jake.morrison@automattic.com>
2019-05-09 15:54:57 +0100

    Fix issue with file requirement

--- classes/class-woothemes-sensei-certificates.php
+++ classes/class-woothemes-sensei-certificates.php
@@ -184,7 +184,7 @@ class WooThemes_Sensei_Certificates {
 		require_once dirname( __FILE__ ) . '/class-woothemes-sensei-certificates.php';
 		require_once dirname( __FILE__ ) . '/class-woothemes-sensei-certificate-templates.php';
 		require_once dirname( __FILE__ ) . '/class-woothemes-sensei-certificates-data-store.php';
-		require_once dirname( __FILE__ ) . 'classes/class-woothemes-sensei-certificates-tfpdf.php';
+		require_once dirname( __FILE__ ) . '/class-woothemes-sensei-certificates-tfpdf.php';
 	}
 
 	/**
