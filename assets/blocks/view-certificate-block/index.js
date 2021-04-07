cb0aa85bca5dbf5c231cddcbe16ab6cd7b0c84ad
Giannis Karagiannis <giannis.karagiannis@automattic.com>
2021-04-07 18:05:06 +0300

    Add prefix to variation name, fix variation title

--- assets/blocks/view-certificate-block/index.js
+++ assets/blocks/view-certificate-block/index.js
@@ -6,18 +6,19 @@ import { registerBlockVariation } from '@wordpress/blocks';
 
 const buttonVariationAttributes = {
 	url: '/course/sensei-view-certificate',
-	title: __( 'View Certificate', 'sensei-certificates' ),
 	text: __( 'View Certificate', 'sensei-certificates' ),
 	linkTarget: '_blank'
 };
 
 registerBlockVariation( 'core/button', {
-	name: 'view-certificate-button',
+	name: 'sensei-certificates/view-certificate-button',
+	title: __( 'View Certificate', 'sensei-certificates' ),
+	description: __( 'Allow a user to view the course certificate.', 'sensei-certificates' ),
 	attributes: buttonVariationAttributes
 } );
 
 registerBlockVariation( 'core/buttons', {
-	name: 'sensei-lms/view-certificate-buttons',
+	name: 'sensei-certificates//view-certificate-buttons',
 	category: 'sensei-lms',
 	description: __( 'Allow a user to view the course certificate. The block is not displayed if the user does not have a certificate.', 'sensei-certificates' ),
 	title: __( 'View Certificate Button', 'sensei-certificates' ),
