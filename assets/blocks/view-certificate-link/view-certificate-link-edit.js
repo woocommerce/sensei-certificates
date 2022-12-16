d65a8cfad240395717842603da7b9fc99f2ba703
Alex Sanford <alex.sanford1@gmail.com>
2022-12-16 10:57:34 -0400

    Add View Certificate Link block

---
+++ assets/blocks/view-certificate-link/view-certificate-link-edit.js
@@ -0,0 +1,38 @@
+/**
+ * WordPress dependencies
+ */
+import { useBlockProps, Warning } from '@wordpress/block-editor';
+import { __ } from '@wordpress/i18n';
+
+/**
+ * Course Overview block edit component.
+ *
+ * @param {Object} props
+ * @param {Object} props.context          Block context.
+ * @param {Object} props.context.postType Post type.
+ */
+const ViewCertificateLinkEdit = ( { context: { postType } } ) => {
+	const blockProps = useBlockProps();
+
+	if ( ! [ 'course', 'lesson' ].includes( postType ) ) {
+		return (
+			<div { ...blockProps }>
+				<Warning>
+					{ __(
+						'The View Certificate Link block can only be used inside the Course List block.',
+						'sensei-certificates'
+					) }
+				</Warning>
+			</div>
+		);
+	}
+
+	return (
+		<div { ...blockProps }>
+			{ /* eslint-disable-next-line jsx-a11y/anchor-is-valid */ }
+			<a href="#">{ __( 'View Certificate', 'sensei-lms' ) }</a>
+		</div>
+	);
+};
+
+export default ViewCertificateLinkEdit;
