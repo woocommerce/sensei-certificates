bfa81768b39c2fb014cc215f7f8e61e95820e62f
Bernat <bernattorras@gmail.com>
2024-11-08 12:07:23 +0100

    Adjust the template check

--- classes/class-woothemes-sensei-certificate-templates.php
+++ classes/class-woothemes-sensei-certificate-templates.php
@@ -162,7 +162,7 @@ class WooThemes_Sensei_Certificate_Templates {
 		}
 
 		// Preview Template
-		add_filter( 'single_template', array( $this, 'certificate_templates_locate_preview_template' ) );
+		add_filter( 'single_template', array( $this, 'certificate_templates_locate_preview_template' ), 10, 3 );
 
 	}
 
@@ -193,16 +193,15 @@ class WooThemes_Sensei_Certificate_Templates {
 	 * @param string $locate locate path
 	 * @return string the location path for the certificate template preview file
 	 */
-	function certificate_templates_locate_preview_template( $locate ) {
+	public function certificate_templates_locate_preview_template( $locate, $type, $templates ) {
 
 		$post_type = get_query_var( 'post_type' );
 
-		if ( 'certificate_template' == $post_type && strpos( $locate, 'single.php' ) ) {
+		if ( 'certificate_template' === $post_type && 'single' === $type ) {
 			$locate = $this->plugin_path() . '/templates/single-certificate_template.php';
 		}
 
 		return $locate;
-
 	}
 
 
